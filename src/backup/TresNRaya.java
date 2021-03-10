db754ce65374469267b7f34b13be765c6ec4d641
programacion <>
2021-03-10 14:07:41 +0100

    haciendo la clase tablero

--- src/backup/TresNRaya.java
+++ src/backup/TresNRaya.java
@@ -1,12 +1,9 @@
-package estructurado;
-
-import modelo.Coordenada;
-import modelo.Tablero;
+package backup;
 
 public class TresNRaya {
 	// El tablero tiene los valores iniciales necesarios par comenzar el juego
 	// Equivale al metodo iniciar juego
-	private Tablero tablero=new Tablero();
+	public int tablero[][] = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
 	public int numerojugada = 2;
 	public int origenx, origeny;
 
@@ -41,10 +38,49 @@ public class TresNRaya {
 
 	}
 
-	
+	/**
+	 * Comprueba si la casilla origen esta ocupada por una ficha de la propiedad del
+	 * turno actual
+	 * 
+	 * @return true si casilla pertenece al turno actual o false en caso contrario
+	 */
+	private boolean comprobarPropiedad() {
+		if (tablero[destinox][destinoy] == verTurno())
+			return true;
 
+		return false;
+	}
 
-	
+	/**
+	 * casilla libre adyacente libre
+	 * 
+	 * @return true si encuentra al menos una casilla libre contigua false si la
+	 *         pieza est� bloqueada
+	 */
+	private boolean comprobarBloqueada() {
+		for (int x = destinox - 1; x <= destinox + 1; x++)
+			for (int y = destinoy - 1; y <= destinoy + 1; y++)
+				if (x > -1 && x < 3 && y > -1 && y < 3)
+					if (tablero[x][y] == 0)
+						return true;
+		return false;
+	}
+
+	/**
+	 * Si en la casilla determinada por las coordenadas x e y se encuentra con un
+	 * cero seignifica que la casilla est� libre
+	 * 
+	 * @param coordenada de fila
+	 * @param coordenada de columna
+	 * @return verdadero si la casilla esta libre y falso en caso contrario
+	 */
+	private boolean mirarCasillaLibre() {
+		if (tablero[destinox][destinoy] == 0) {
+			return true;
+		}
+
+		return false;
+	}
 
 	/**
 	 * Va a pedir posiciones de la ficha que queremos colocar hasta que hayamos
@@ -59,9 +95,9 @@ public class TresNRaya {
 		// haya metido la casilla origen
 		if (numerojugada > 6) {
 			if (!mover) {
-				if (casillaContigua() && tablero.mirarCasillaLibre(new Coordenada(destinox, destinoy))) {
-					tablero.setValorPosicion(new Coordenada(origenx, origeny),0);
-					tablero.setValorPosicion(new Coordenada(destinox, destinoy),verTurno());
+				if (casillaContigua() && mirarCasillaLibre()) {
+					tablero[origenx][origeny] = 0;
+					tablero[destinox][destinoy] = verTurno();
 					mover = true;
 					numerojugada++;
 					return true;
@@ -70,7 +106,7 @@ public class TresNRaya {
 					return false;
 			} // if(mover)
 			else {
-				if (tablero.comprobarPropiedad(new Coordenada(destinox, destinoy),verTurno()) && tablero.comprobarBloqueada(new Coordenada(destinox, destinoy))) {
+				if (comprobarPropiedad() && comprobarBloqueada()) {
 					origenx = destinox;
 					origeny = destinoy;
 					mover = false;
@@ -80,8 +116,8 @@ public class TresNRaya {
 					return false;
 			} // if(!mover&&!colocar){
 		} // if (numerojugada>6){
-		else if (tablero.mirarCasillaLibre(new Coordenada(destinox, destinoy))) {
-			tablero.setValorPosicion(new Coordenada(destinox, destinoy),verTurno());
+		else if (mirarCasillaLibre()) {
+			tablero[destinox][destinoy] = verTurno();
 			incrementaJugada();
 			// Si ha modificado el tablero
 			return true;
@@ -92,6 +128,8 @@ public class TresNRaya {
 
 	}// public void realizarJugada(){
 
+	// Si el numero de jugada es par el turno es del jugador 2 y si no del jugador 1
+
 	/**
 	 * Su cometido es controlar el numero de jugada que llevamos
 	 * 
@@ -100,9 +138,54 @@ public class TresNRaya {
 		numerojugada++;
 	}
 
-	
+	/**
+	 * Mirar las filas del tablero
+	 * 
+	 * @return true si hay tres fichas de la misma clase en una fila
+	 */
+	private boolean compruebaFila() {
+		for (int x = 0; x < 3; x++)
+			if (tablero[x][0] == tablero[x][1] && tablero[x][1] == tablero[x][2] && tablero[x][1] != 0)
+				return true;
+		return false;
+	}
 
-	
+	/**
+	 * Mira las columnas del tablero
+	 * 
+	 * @return true si tres fichas de la misma clase en una columna
+	 */
+	private boolean compruebaColumna() {
+		for (int y = 0; y < 3; y++)
+			if (tablero[0][y] == tablero[1][y] && tablero[1][y] == tablero[2][y] && tablero[1][y] != 0)
+				return true;
+		return false;
+	}
+
+	/**
+	 * Comprueba si alguna diagonal tiene tres en raya No hace falta comparar si
+	 * est�n vacias, no puede ocurrir
+	 * 
+	 * @return true si se da la circunstancia
+	 */
+	private boolean compruebaDiagonal() {
+		if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2])
+			return true;
+		if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])
+			return true;
+		return false;
+	}
+
+	/**
+	 * Se encarga de llamar a los metodos que comprueban las tres en raya
+	 * 
+	 * @return true si se han conseguida tres en raya
+	 */
+	public boolean comprobarTresEnRaya() {
+		if (compruebaFila() || compruebaColumna() || compruebaDiagonal())
+			return true;
+		return false;
+	}
 
 	/**
 	 * Esta funcion retorna una "X" cuando se le pasa un 2 por parametro, una "O"
@@ -141,17 +224,17 @@ public class TresNRaya {
 
 	public String indicarAnomalia() {
 		if (numerojugada <= 6) {
-			if (!tablero.mirarCasillaLibre(new Coordenada(destinox, destinoy)))
+			if (!mirarCasillaLibre())
 				return "La casilla no est� libre";
 		} else {
 			if (mover) {
-				if (!tablero.comprobarPropiedad(new Coordenada(destinox, destinoy),verTurno()) )
+				if (!comprobarPropiedad())
 					return "La pieza elegida no es correcta";
-				if (!tablero.comprobarBloqueada(new Coordenada(destinox, destinoy)))
+				if (!comprobarBloqueada())
 					return "La pieza seleccionada esta bloqueada";
 			} // if
 			else {
-				if (!tablero.mirarCasillaLibre(new Coordenada(destinox, destinoy)))
+				if (!mirarCasillaLibre())
 					return "La casilla no est� libre";
 				if (!casillaContigua())
 					return "Casilla no contigua";
